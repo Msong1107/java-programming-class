7d729c486cdef197bf6a58f916a8f3c52c1b209b
문송 <84947346+Msong1107@users.noreply.github.com>
2021-09-07 20:32:50 +0900

    Add files via upload

---
+++ do_while문.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class do_while¹® {
+	public static void main(String[] args) {
+	int a = 1;
+		do {
+		 System.out.printf("%d",a);
+		 a++; }
+		while(a <5);
+	}
+}
---
+++ while문.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class while¹® {
+	public static void main(String[] args) {
+	int a = 1;
+	 while(a < 5) {
+		 System.out.printf("%d",a);
+		 a++; 
+	 }
+	}
+}
