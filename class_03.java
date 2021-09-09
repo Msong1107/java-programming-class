dc5f60ed5184bc28f8b7fcbd39284a003fef0980
문송 <84947346+Msong1107@users.noreply.github.com>
2021-09-09 20:42:18 +0900

    Add files via upload

---
+++ class_03.java
@@ -0,0 +1,19 @@
+
+public class class_03 {
+	public static void main (String[] args) {
+		int a = 0;
+		double b;
+		
+		System.out.print(b);
+		System.out.print(a+c);
+		
+		int c= 0;
+		
+		pulbic double d = 0.0;
+		
+		for(int e= 0; e<10; e++) {
+			int a= 1;
+			System.out.print(e);
+		}
+	}
+}
