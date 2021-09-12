642e400c3cbd964b90cdf594bc60ebc1e92959e8
문송 <84947346+Msong1107@users.noreply.github.com>
2021-09-13 07:52:50 +0900

    Add files via upload

---
+++ circle.java
@@ -0,0 +1,23 @@
+class Circle2{
+	double radius;
+	String color;
+	
+	public Circle2(double radius, String color){
+		this.radius = radius;
+		this.color = color;
+	}
+	public Circle2(double radius) {
+		this(radius,"ÆÄ¶û");
+	}
+	
+	public Circle2(double color) {
+		this(10.0,color);
+	}
+	
+	public Circle2() {
+		this(10.0,"»¡°­");
+	}
+	
+	
+	}
+}
